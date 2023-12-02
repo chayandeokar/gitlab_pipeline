d70b2378e6e6e9ad693606624c6e362742e36982
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-12-02 23:20:00 +0530

    Create footprinting.go

---
+++ footprinting.go
@@ -0,0 +1,22 @@
+Footprinting in Cryptography:
+
+Footprinting is the process of collecting information about a target system, network, or organization to understand its structure and vulnerabilities. In the cryptographic context, footprinting may involve reconnaissance activities to gather data that could be exploited for unauthorized access or attacks:
+
+Network Footprinting:
+
+Identifying and mapping the network architecture, including IP addresses, subnets, and network devices. This information can be used to plan targeted attacks.
+DNS Footprinting:
+
+Gathering information about a domain's DNS records, including name servers, mail servers, and other domain-related data. This helps in understanding the target's online presence.
+Website Footprinting:
+
+Analyzing a website's structure, content, and technologies used. This can reveal potential vulnerabilities and aid in planning attacks.
+Social Engineering Footprinting:
+
+Collecting information about individuals within an organization through social engineering techniques. This may involve exploiting human psychology to gain access to sensitive data.
+Email Footprinting:
+
+Analyzing email headers and metadata to gather information about email servers, routing, and potential vulnerabilities.
+Competitive Intelligence Footprinting:
+
+Collecting information about competitors, partners, or industry trends that may impact the target organization's security.
