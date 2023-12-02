1dc4fe36bdfdfd828673ba8efe9d2dd0d4176884
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-12-02 23:19:32 +0530

    Create Spoofing.go

---
+++ Spoofing.go
@@ -0,0 +1,20 @@
+Spoofing in Cryptography:
+
+Spoofing is a deceptive technique where an attacker pretends to be someone or something else to gain unauthorized access or deceive users. In the context of cryptography, spoofing can manifest in various forms:
+
+IP Spoofing: The attacker manipulates the source address in the IP header to make it appear as if the data packets are coming from a trusted source. This can be exploited to bypass security mechanisms.
+Email Spoofing:
+
+Spoofing the sender's email address to make it appear as if the email is coming from a legitimate source. This technique is often used in phishing attacks to deceive recipients.
+Website Spoofing:
+
+Creating a fake website that mimics a legitimate one to trick users into providing sensitive information, such as login credentials or financial details.
+DNS Spoofing:
+
+Manipulating the Domain Name System (DNS) to redirect users to malicious websites by providing false IP address mappings.
+MAC Address Spoofing:
+
+Changing the Media Access Control (MAC) address of a network interface to impersonate a different device on a network.
+Biometric Spoofing:
+
+Attempting to fool biometric authentication systems by using fake fingerprints, facial images, or other biometric data.
